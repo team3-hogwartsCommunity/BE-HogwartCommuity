e568df73389807be8843dded4affa2e71e865686
Wooram Hong <coadhs1@naver.com>
2023-03-01 18:20:43 +0900

    Refactor: successCode 수정 #31

--- src/main/java/com/sparta/spartaminiproject/common/utill/SuccessCode.java
+++ src/main/java/com/sparta/spartaminiproject/common/utill/SuccessCode.java
@@ -9,8 +9,19 @@ import org.springframework.http.HttpStatus;
 public enum SuccessCode {
     SIGNUP_SUCCESS(HttpStatus.OK, "회원가입 성공"),
     LOGIN_SUCCESS(HttpStatus.OK, "로그인 성공"),
+
+    BLOG_POST_SUCCESS(HttpStatus.OK,"게시글 작성 성공"),
+    BLOG_PUT_SUCCESS(HttpStatus.OK,"게시글 수정 성공"),
     BLOG_DELETE_SUCCESS(HttpStatus.OK,"게시글 삭제 성공"),
+
+    COMMENT_POST_SUCCESS(HttpStatus.OK,"댓글 작성 성공"),
+    COMMENT_PUT_SUCCESS(HttpStatus.OK,"댓글 수정 성공"),
     COMMENT_DELETE_SUCCESS(HttpStatus.OK,"댓글 삭제 성공"),
+
+    RECOMMENT_POST_SUCCESS(HttpStatus.OK, "대댓글 작성 성공"),
+    RECOMMENT_PUT_SUCCESS(HttpStatus.OK, "대댓글 수정 성공"),
+    RECOMMENT_DELETE_SUCCESS(HttpStatus.OK, "대댓글 삭제 성공"),
+
     LIKE_SUCCESS(HttpStatus.OK, "좋아요 선택"),
     NOT_LIKE_SUCCESS(HttpStatus.OK, "좋아요 취소");
 
